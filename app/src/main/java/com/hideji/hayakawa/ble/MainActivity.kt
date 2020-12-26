a92375b6988c041d2755e5192d098168c9e64127
hideji <hideji.hayakawa@hexagon.com>
2020-12-26 19:37:33 -0300

    removed service

--- app/src/main/java/com/hideji/hayakawa/ble/MainActivity.kt
+++ app/src/main/java/com/hideji/hayakawa/ble/MainActivity.kt
@@ -1,49 +1,24 @@
 package com.hideji.hayakawa.ble
 
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothGattCharacteristic
-import android.bluetooth.BluetoothGattDescriptor
-import android.bluetooth.BluetoothManager
+import android.bluetooth.*
 import android.content.*
 import android.os.BatteryManager
 import android.os.Bundle
-import android.os.IBinder
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var mConnected = false
-    private var mBluetoothLeService: BluetoothLeService? = null
     private var mDeviceAddress: String? = "D4:36:39:6B:97:67"
+    private lateinit var mBluetoothManager: BluetoothManager
+    private lateinit var mBluetoothAdapter: BluetoothAdapter
+    private lateinit var mBluetoothDevice: BluetoothDevice
+    private lateinit var mBluetoothGatt: BluetoothGatt
+    private lateinit var mMessage: String
 
     companion object {
 
-        private fun makeGattUpdateIntentFilter(): IntentFilter {
-            val intentFilter = IntentFilter()
-            intentFilter.addAction(BluetoothLeService.ACTION_GATT_CONNECTED)
-            intentFilter.addAction(BluetoothLeService.ACTION_GATT_DISCONNECTED)
-            intentFilter.addAction(BluetoothLeService.ACTION_GATT_SERVICES_DISCOVERED)
-            intentFilter.addAction(BluetoothLeService.ACTION_DATA_AVAILABLE)
-            return intentFilter
-        }
-    }
-
-    private val mServiceConnection = object : ServiceConnection {
-
-        override fun onServiceConnected(componentName: ComponentName, service: IBinder) {
-            mBluetoothLeService = (service as BluetoothLeService.LocalBinder).service
-            if (!mBluetoothLeService!!.initialize()) {
-                finish()
-            }
-            // Automatically connects to the device upon successful start-up initialization.
-            mBluetoothLeService!!.connect(mDeviceAddress)
-        }
-
-        override fun onServiceDisconnected(componentName: ComponentName) {
-            mBluetoothLeService = null
-        }
     }
 
     private val batteryReceiver = object : BroadcastReceiver(){
@@ -51,34 +26,24 @@ class MainActivity : AppCompatActivity() {
             val level: Int = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
             val scale: Int = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
 
-            if (mConnected) {
-                if (level * 100 / scale.toFloat() > 84) {
-                    writeCharacteristic("desligar\n")
-                } else if (level * 100 / scale.toFloat() < 50) {
-                    writeCharacteristic("ligar\n")
-                }
+            if (level * 100 / scale.toFloat() > 84) {
+                writeCharacteristic("desligar\n")
+            } else if (level * 100 / scale.toFloat() < 50) {
+                writeCharacteristic("ligar\n")
             }
         }
     }
 
-    private fun writeCharacteristic(value : String)
-    {
-        if (mConnected) {
-            val characteristic: BluetoothGattCharacteristic? =
-                mBluetoothLeService?.mBluetoothGatt?.getService(UUID.fromString("0000ffe0-0000-1000-8000-00805f9b34fb"))
-                    ?.getCharacteristic(
-                        UUID.fromString("0000ffe1-0000-1000-8000-00805f9b34fb")
-                    )
-            characteristic?.setValue(value)
-            mBluetoothLeService?.mBluetoothGatt?.writeCharacteristic(characteristic)
-        }
+    private fun writeCharacteristic(value : String) {
+        mMessage = value
+        mBluetoothGatt = mBluetoothDevice.connectGatt(this, false, mGattCallback)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val batteryStatus: Intent? = registerReceiver(batteryReceiver,
+        registerReceiver(batteryReceiver,
             IntentFilter(Intent.ACTION_BATTERY_CHANGED)
         )
 
@@ -90,24 +55,29 @@ class MainActivity : AppCompatActivity() {
             writeCharacteristic("desligar\n")
         }
 
-        val gattServiceIntent = Intent(this, BluetoothLeService::class.java)
-        bindService(gattServiceIntent, mServiceConnection, Context.BIND_AUTO_CREATE)
-        registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter())
+        mBluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        mBluetoothAdapter = mBluetoothManager!!.adapter
+        mBluetoothDevice = mBluetoothAdapter!!.getRemoteDevice(mDeviceAddress)
     }
 
-    private val mGattUpdateReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent) {
-            when (intent.action) {
-                BluetoothLeService.ACTION_GATT_CONNECTED -> {
-                    mConnected = true
-                    invalidateOptionsMenu()
-                }
-                BluetoothLeService.ACTION_GATT_DISCONNECTED -> {
-                    mConnected = false
-                    invalidateOptionsMenu()
-                }
+    private val mGattCallback = object : BluetoothGattCallback() {
+        override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                mBluetoothGatt.discoverServices()
+            }
+        }
+
+        override fun onServicesDiscovered(gatt: BluetoothGatt, status: Int) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                val characteristic: BluetoothGattCharacteristic? =
+                    mBluetoothGatt?.getService(UUID.fromString("0000ffe0-0000-1000-8000-00805f9b34fb"))
+                        ?.getCharacteristic(
+                            UUID.fromString("0000ffe1-0000-1000-8000-00805f9b34fb")
+                        )
+                characteristic?.setValue(mMessage)
+                mBluetoothGatt?.writeCharacteristic(characteristic)
+                mBluetoothGatt.disconnect()
             }
         }
     }
-
 }
